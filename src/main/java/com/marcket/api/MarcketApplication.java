798689bb0393aa096210ab03b1520250ff1a4f3c
Santi Rodriguez <santimanuelr@gmail.com>
2024-06-05 23:47:15 -0300

    initial structure

---
+++ src/main/java/com/marcket/api/MarcketApplication.java
@@ -0,0 +1,13 @@
+package com.marcket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcketApplication.class, args);
+	}
+
+}
